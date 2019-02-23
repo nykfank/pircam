142a95d91a8c17cfb0052c110b99b63bd5d4ca39
nick@nyk.ch <nick@nyk.ch>
2019-02-23 23:30:31 +0100

    commit

--- bitrate_db.py
+++ bitrate_db.py
@@ -22,19 +22,26 @@ def load_config(fn, section):
 				cdict[device][key] = value
 	return cdict
 
-def get_bitrate(fn):
-	cmd = 'ffprobe %s 2>&1' % fn
-	r = os.popen(cmd).read()
-	if not 'bitrate: ' in r: return 0
-	return int(r.split('bitrate: ')[1].split(' ')[0].strip())
+def get_rate(mfn, ofn):
+	return int(100 * float(os.stat(ofn).st_size) / float(os.stat(mfn).st_size))
 
-def bitrate_to_db(fn, bitrate):
+def rate_to_db(fn, rate):
 	db = sqlite3.connect(config['db_fn'])
 	dbc = db.cursor()
 	fn2 = fn.replace(config['data_dir'] + '/', '')
-	dbc.execute('INSERT INTO files (fn, bitrate) VALUES (?,?)',(fn2, bitrate))
+	dbc.execute('INSERT INTO files (fn, rate) VALUES (?,?)',(fn2, rate))
 	db.commit()
 	db.close()
+
+def rate_from_db(fn):
+	db = sqlite3.connect(config['db_fn'])
+	dbc = db.cursor()
+	fn2 = fn.replace(config['data_dir'] + '/', '')
+	dbc.execute('SELECT rate FROM files WHERE fn = ?', (fn2,))
+	r = dbc.fetchone()
+	db.close()
+	if not r: return 0
+	return r[0]
 
 def exists_in_db(fn):
 	db = sqlite3.connect(config['db_fn'])
@@ -50,10 +57,12 @@ cameras = load_config(config_file, 'CAMERA')
 config  = load_config(config_file, 'GLOBAL')
 for CAMID in cameras.keys():
 	indir = '%s/%s' % (config['data_dir'], CAMID)
+	indir_mjpeg = '%s/%s_mjpeg' % (config['data_dir'], CAMID)
 	for f in os.listdir(indir):
 		if not f.endswith('ogg'): continue
 		fn = '%s/%s' % (indir, f)
+		mfn = '%s/%s' % (indir_mjpeg, f.replace('ogg', 'mjpeg'))
 		if exists_in_db(fn): continue
-		bitrate = get_bitrate(fn)
-		bitrate_to_db(fn, bitrate)
-		print '%s: %d' % (fn, bitrate)
+		rate = get_rate(mfn, fn)
+		rate_to_db(fn, rate)
+		print '%s: %d' % (fn, rate)
