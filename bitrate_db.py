bc646f634de6f53c0f9d80eccdfb72f5242e9e99
nick@nyk.ch <nick@nyk.ch>
2019-02-23 01:17:10 +0100

    commit

--- bitrate_db.py
+++ bitrate_db.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python
-# Better use number of frames from mjpeg and size of compressed ogg video?
-
-import sqlite3, os, sys
-indir = sys.argv[1]
+import sqlite3, os
+config_file = '/opt/pircam_config.txt'
 db_fn = '%s/.bitrate.db' % os.getenv("HOME")
+
+def load_config(fn, section):
+	"""Load specified section of config file into dictionary."""
+	cdict, current_section = {}, ''
+	for i in open(fn):
+		if not i.strip(): continue
+		isp = i.strip().split('=')
+		if len(isp) == 1:
+			title = isp[0].strip()
+			if title in ['GLOBAL', 'CAMERA', 'SENSOR']: current_section = title
+			else: device = title
+		if current_section != section: continue
+		if len(isp) == 2:
+			key, value = isp[0].strip(), isp[1].strip()
+			if current_section == 'GLOBAL': 
+				cdict[key] = value
+			else:
+				if not cdict.has_key(device): cdict[device] = {}
+				cdict[device][key] = value
+	return cdict
+
+# Load configuration into dictionaries
+cameras = load_config(config_file, 'CAMERA')
+config  = load_config(config_file, 'GLOBAL')
 db = sqlite3.connect(db_fn)
 dbc = db.cursor()
 dbc.execute('CREATE TABLE IF NOT EXISTS files (fn TEXT, bitrate INTEGER)')
 dbc.execute('CREATE INDEX IF NOT EXISTS files_idx ON files (fn)')
-for f in os.listdir(indir):
-	if not f.endswith('ogg'): continue
-	fn = '%s/%s' % (indir, f)
-	cmd = 'ffprobe %s 2>&1' % fn
-	print cmd
-	r = os.popen(cmd).read()
-	print r
-	fn2 = fn.replace('/home/', '')
-	r = r.split('bitrate: ')[1].split(' ')[0]
-	dbc.execute('INSERT INTO files (fn, bitrate) VALUES (?,?)',(fn2, r.strip()))
+for CAMID in cameras.keys():
+	indir = '%s/%s' % (config['data_dir'], CAMID)
+	for f in os.listdir(indir):
+		if not f.endswith('ogg'): continue
+		fn = '%s/%s' % (indir, f)
+		fn2 = fn.replace(config['data_dir'], '')
+		dbc.execute('SELECT COUNT(*) FROM files WHERE fn = ?', (fn2,))
+		if dbc.fetchone()[0] > 0: continue
+		cmd = 'ffprobe %s 2>&1' % fn
+		r = os.popen(cmd).read()
+		if not 'bitrate: ' in r:
+			print '%s: ERROR' % fn
+			continue
+		bitrate = int(r.split('bitrate: ')[1].split(' ')[0].strip())
+		dbc.execute('INSERT INTO files (fn, bitrate) VALUES (?,?)',(fn2, bitrate))
+		print '%s: %d' % (fn, bitrate)
 	db.commit()
