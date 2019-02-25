bad27e978e131d570fc7550173c66ddda6e8f664
nick@nyk.ch <nick@nyk.ch>
2019-02-26 00:57:42 +0100

    commit

--- netcam_movie_all.R
+++ netcam_movie_all.R
@@ -1,12 +1,10 @@
 # args: eingang / hinterhaus
 args <- commandArgs(trailingOnly=TRUE)
-if (!exists("camID")) camID <- args[1]
+indir <- args[1]
 library(data.table)
 library(suncalc)
-indir <- sprintf("/mnt/big/nick/cams/%s", camID)
 writeLines(sprintf("Movie directory: %s", indir))
 tempTextFile <- "/tmp/vidfiles.txt"
-sampleSize <- 50
 video <- data.table(datei = list.files(indir, pattern="ogg"))
 video[, zeit := as.POSIXct(sub("netcam", "", datei), tz="", "%Y%m%d_%H%M%S")]
 video[, stunde := as.numeric(format(zeit, "%H")) + as.numeric(format(zeit, "%M")) / 60]
@@ -15,19 +13,13 @@ video[, sunrise := as.numeric(format(sunrise, "%H")) + as.numeric(format(sunrise
 video[, sunsetStart := getSunlightTimes(date = as.Date(zeit), lat = 46.93, lon = 7.415, tz = "CET")$sunsetStart]
 video[, sunsetStart := as.numeric(format(sunsetStart, "%H")) + as.numeric(format(sunsetStart, "%M")) / 60]
 video[, tag := stunde > sunrise & stunde < sunsetStart]
-video[tag==TRUE, id := 1:nrow(video[tag==TRUE,])]
-video[tag==TRUE, wahl := id %in% sample(1:nrow(video[tag==TRUE,]), sampleSize)]
-video[is.na(wahl), "wahl"] <- FALSE
-print(table(video$tag, video$wahl, dnn=c("tag", "wahl")))
-
-mergefiles <- sprintf("file '%s/%s'", indir, video[wahl==TRUE, datei])
+print(table(video$tag, dnn=c("tag")))
+mergefiles <- sprintf("file '%s/%s'", indir, video[tag==TRUE, datei])
 write(mergefiles, file=tempTextFile)
-outfile <- sprintf("%s%d.mp4", camID, as.integer(Sys.time()))
+outfile <- sprintf("merged_%s.mp4", basename(indir))
+writeLines(outfile)
 cmd <- sprintf(
-	'ffmpeg -hide_banner -loglevel panic -f concat -safe 0 -i %s -filter:v "setpts=0.25*PTS" -filter:a "atempo=2.0,atempo=2.0" %s', 
+	'ffmpeg -y -hide_banner -loglevel panic -f concat -safe 0 -i %s -filter:v "setpts=0.25*PTS" -filter:a "atempo=2.0,atempo=2.0" %s', 
 	tempTextFile, outfile
 )
 system(cmd)
-writeLines(outfile)
-
-# TODO: use mediainfo filaname.ogg to get length / compression ratio
