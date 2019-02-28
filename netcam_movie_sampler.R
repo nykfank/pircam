37e6cc02e839a18d98e90be53ae5ef12a4e9c6be
nick@nyk.ch <nick@nyk.ch>
2019-03-01 00:16:25 +0100

    commit

--- netcam_movie_sampler.R
+++ netcam_movie_sampler.R
@@ -7,32 +7,35 @@ library(suncalc)
 indir <- sprintf("/mnt/big/nick/cams/%s", camID)
 writeLines(sprintf("Movie directory: %s", indir))
 tempTextFile <- sprintf("/tmp/vidfiles%d.txt", as.integer(Sys.time()))
-sampleSize <- 20
+sampleSize <- 60
+seconds_per_video <- 4
 video <- data.table(datei = list.files(indir, pattern="ogg"))
 stopifnot(nrow(video) > 0)
 video[, zeit := as.POSIXct(sub("netcam", "", datei), tz="", "%Y%m%d_%H%M%S")]
+video <- video[zeit > as.POSIXct('2019-01-28'),] # No audio before this date
 video[, stunde := as.numeric(format(zeit, "%H")) + as.numeric(format(zeit, "%M")) / 60]
 video[, sunrise := getSunlightTimes(date = as.Date(zeit), lat = 46.93, lon = 7.415, tz = "CET")$sunrise]
 video[, sunrise := as.numeric(format(sunrise, "%H")) + as.numeric(format(sunrise, "%M")) / 60]
 video[, sunsetStart := getSunlightTimes(date = as.Date(zeit), lat = 46.93, lon = 7.415, tz = "CET")$sunsetStart]
 video[, sunsetStart := as.numeric(format(sunsetStart, "%H")) + as.numeric(format(sunsetStart, "%M")) / 60]
 video[, tag := stunde > sunrise & stunde < sunsetStart]
-video[tag==TRUE, id := 1:nrow(video[tag==TRUE,])]
-video[tag==TRUE, wahl := id %in% sample(1:nrow(video[tag==TRUE,]), sampleSize)]
+video[tag==TRUE, id := 1:nrow(video[tag == TRUE,])]
+video[tag==TRUE, wahl := id %in% sample(1:nrow(video[tag == TRUE,]), sampleSize)]
 video[is.na(wahl), "wahl"] <- FALSE
 print(table(video$tag, video$wahl, dnn=c("tag", "wahl")))
 
-mergefiles <- sprintf("file '%s/%s'", indir, video[wahl==TRUE, datei])
+for (v in video[wahl==TRUE, datei]) {
+	cmd <- sprintf("ffmpeg -hide_banner -loglevel panic -y -i %s/%s -to 00:00:%02d -c copy /tmp/%s", indir, v, seconds_per_video, v)
+	writeLines(cmd)
+	system(cmd)
+}
+
+mergefiles <- sprintf("file '/tmp/%s'", video[wahl==TRUE, datei])
 write(mergefiles, file=tempTextFile)
 outfile <- sprintf("%s%d.ogg", camID, as.integer(Sys.time()))
 cmd <- sprintf(
-	'ffmpeg -hide_banner -f concat -safe 0 -i %s -filter:v "setpts=0.25*PTS" -filter:a "atempo=2.0,atempo=2.0" -qscale:v 7 %s', 
+	'ffmpeg -hide_banner -loglevel panic -y -f concat -safe 0 -i %s -filter:v "setpts=0.25*PTS" -filter:a "atempo=2.0,atempo=2.0" -qscale:v 7 %s', 
 	tempTextFile, outfile
 )
+writeLines(cmd)
 system(cmd)
-writeLines(outfile)
-
-# -loglevel panic
-# -vf select="between(n\\,0\\,10)" -vsync 0 
-# write frames to images, create movie from first x number of frames?
-# concatenate audio or pass to ffmpeg when creating movie from frames?
