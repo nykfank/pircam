c8d3cf586699e2ba722265b186ccb4e7ccd7f554
nick@nyk.ch <nick@nyk.ch>
2019-03-09 01:52:55 +0100

    commit

---
+++ hvLines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import Image, sys, math, numpy, scipy.stats
+ifn = sys.argv[1]
+i = Image.open(ifn)
+s = i.size
+im = Image.new('RGB', s)
+for x in range(s[0]):
+	for y in range(s[1]):
+		p = i.getpixel((x, y))
+		if y > 1:
+			p1 = i.getpixel((x, y-1))
+			p2 = i.getpixel((x, y))
+			d = int(math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2))
+			im.putpixel((x,y), (d,d,d))
+
+im.save('foo.png')
+ddl = []
+for y in range(s[1]):
+	dd = 0
+	for x in range(s[0]): dd += im.getpixel((x, y))[0]
+	ddl.append(dd/s[0])
+sddl = sorted(ddl)
+if sddl[-1] - sddl[-2] > scipy.stats.iqr(ddl): print 'Line'
