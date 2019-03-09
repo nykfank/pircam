20c5e39c7601c0bba9b8a30cb35071b359246a7f
nick@nyk.ch <nick@nyk.ch>
2019-03-09 15:27:37 +0100

    commit

--- hvLines.py
+++ hvLines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import Image, sys, math, numpy, scipy.stats
+import Image, sys, math, numpy, scipy.stats, shutil
 ifn = sys.argv[1]
 i = Image.open(ifn)
 s = i.size
@@ -20,4 +20,11 @@ for y in range(s[1]):
 	for x in range(s[0]): dd += im.getpixel((x, y))[0]
 	ddl.append(dd/s[0])
 sddl = sorted(ddl)
-if sddl[-1] - sddl[-2] > scipy.stats.iqr(ddl): print 'Line'
+iqr = scipy.stats.iqr(ddl)
+median = numpy.median(ddl)
+#print 'Median:', median
+#print 'IQR:', iqr
+#print sddl
+lines = filter(lambda x : x > median + 4 * iqr, sddl)
+print ifn, lines
+if len(lines) > 0: shutil.copy(ifn, 'line/'+ifn)
