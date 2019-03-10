5eadceee3af28a52ba85ba1fadea280f88148b28
nick@nyk.ch <nick@nyk.ch>
2019-03-10 01:30:40 +0100

    commit

--- hvLines.py
+++ hvLines.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 import sys, numpy, scipy.stats, shutil, cv2, os
 thr = 4
-odir = '/tmp/broken/'
+odir = '/tmp/broken'
 if not os.path.isdir(odir): os.mkdir(odir)
+indir = sys.argv[1]
 
-# Load image and convert to 64bit integer
-ifn = sys.argv[1]
-img = cv2.imread(ifn, cv2.IMREAD_GRAYSCALE)
-img = numpy.int64(img)
+def find_jpeg_artefacts(ifn):
+	# Load image and convert to 64bit integer
+	img = cv2.imread(ifn, cv2.IMREAD_GRAYSCALE)
+	img = numpy.int64(img)
+	# Horizontal artefacts
+	img_downshifted = numpy.roll(img, 1, axis = 0) # Shift down
+	diff_down = abs(numpy.subtract(img, img_downshifted))
+	row_sum = numpy.sum(diff_down, axis=1) / img.shape[0]
+	row_sum[0] = 0 # Top row of shifted image is bottom row
+	return [ i for i, x in enumerate(row_sum) if x > numpy.median(row_sum) + thr * scipy.stats.iqr(row_sum) ]
 
-# Horizontal artefacts
-img_downshifted = numpy.roll(img, 1, axis = 0) # Shift down
-diff_down = abs(numpy.subtract(img, img_downshifted))
-row_sum = numpy.sum(diff_down, axis=1) / img.shape[0]
-row_sum[0] = 0 # Top row of shifted image is bottom row
-artefact_rows = [ i for i, x in enumerate(row_sum) if x > numpy.median(row_sum) + thr * scipy.stats.iqr(row_sum) ]
-
-print ifn, artefact_rows
-if len(artefact_rows) > 0: shutil.move(ifn, odir + ifn)
+for fn in os.listdir(indir):
+	fpath = '%s/%s' % (indir, fn)
+	artefact_rows = find_jpeg_artefacts(fpath)
+	print fn, artefact_rows
+	if len(artefact_rows) > 0: shutil.move(fpath, '%s/%s' % (odir, ifn))
