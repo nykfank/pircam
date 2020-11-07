98d32bbb7e062cb27155f4198501007976c94b25
nick@nyk.ch <nick@nyk.ch>
2020-11-07 01:29:27 +0100

    commit

--- tinker_raspi_relais.py
+++ tinker_raspi_relais.py
@@ -6,9 +6,13 @@ from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
 import time, signal
 
 tinkerforge_address = '192.168.1.218'
-tinkerforge_uid = 'MLF'
-Relay_Ch2 = 20 # IR LED
+tinkerforge_uid = 'MLF' # Motion Sensor 2.0
+Relay_Ch1 = 26
+Relay_Ch2 = 20 # Relais with IR floodlight attached
+Relay_Ch3 = 21 # Relais with Camera attached
 event_lock = False
+reset_period = 60*60*12
+reset_timer = reset_period
 log_fn = '/home/pi/tinker_raspi_relais.log'
 
 def logg(x):
@@ -29,10 +33,10 @@ def activate_relais():
     if event_lock == True: return
     event_lock = True
     GPIO.output(Relay_Ch2, GPIO.LOW)
-    logg('relais ON')
+    logg('relais2 ON')
     time.sleep(30)
     GPIO.output(Relay_Ch2, GPIO.HIGH)
-    logg('relais OFF')
+    logg('relais2 OFF')
     event_lock = False
 
 killer = GracefulKiller()
@@ -43,12 +47,23 @@ sensor.register_callback(sensor.CALLBACK_MOTION_DETECTED, activate_relais) # Reg
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Relay_Ch2, GPIO.OUT)
+GPIO.setup(Relay_Ch3, GPIO.OUT)
+GPIO.output(Relay_Ch3, GPIO.LOW)
+logg('relais3 ON')
 logg('tinker_raspi_relais started')
 
 # Event loop
 while True:
     if event_lock == False and killer.kill_now: break # Terminate
     time.sleep(5)
+    reset_timer = reset_timer - 5
+    if event_lock == False and reset_timer < 0:
+        GPIO.output(Relay_Ch3, GPIO.HIGH)
+        logg('relais3 OFF')
+        time.sleep(30)
+        GPIO.output(Relay_Ch3, GPIO.LOW)
+        logg('relais3 ON')
+        reset_timer = reset_period
 GPIO.cleanup()
 ipc.disconnect()
 logg('tinker_raspi_relais ended')
