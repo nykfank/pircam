e7935698b81db4506c6172e497a7a84b541f0edb
nykfank <nick@nyk.ch>
2025-01-19 23:26:54 +0100

    Random selection of sound effect from a directory of wav files.

--- dog.py
+++ dog.py
@@ -2,12 +2,13 @@
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_motion_detector import BrickletMotionDetector
 from tinkerforge.bricklet_motion_detector_v2 import BrickletMotionDetectorV2
-import time, subprocess,  os, signal
+import time, subprocess, os, signal, random
 tinkerforge_uid = 'MLF'
 tinkerforge_address = '192.168.1.209' # Server to fetch videos
 verbose = True
 event_lock = False
 log_fn = '/home/pi/sensodog.log'
+wavdir = '/home/pi/wav'
 
 def logg(x):
     """Writes message and formated timestamp to logfile."""
@@ -32,7 +33,10 @@ def play_sound():
     global event_lock
     if event_lock == True: return
     event_lock = True
-    cmd = '/usr/bin/aplay', '/home/pi/wav/bestdog.wav'
+    wavfiles = os.listdir(wavdir)
+    idx = int(random.random() * len(wavfiles))
+    randwav = '%s/%s' % (wavdir, wavfiles[idx])
+    cmd = '/usr/bin/aplay', randwav
     log_and_run(cmd)
     event_lock = False
 
@@ -44,7 +48,7 @@ sensor.register_callback(sensor.CALLBACK_MOTION_DETECTED, play_sound) # Register
 logg('sensodog started')
 # Event loop
 while True:
-    time.sleep(5)
+    time.sleep(1)
     if killer.kill_now: break # Terminate
 ipc.disconnect()
 logg('sensodog ended')
